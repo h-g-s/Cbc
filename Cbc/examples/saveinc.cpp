563c9ef2287b527c1977697780b4e5915e2defc0
Haroldo <haroldo.santos@gmail.com>
2019-02-04 17:55:14 +0100

    source formatting

--- Cbc/examples/saveinc.cpp
+++ Cbc/examples/saveinc.cpp
@@ -7,8 +7,8 @@
 #include <Cbc_C_Interface.h>
 #include "CbcEventHandler.hpp"
 
-static int callBack(CbcModel * model, int whereFrom)
-{/*
+static int callBack(CbcModel *model, int whereFrom)
+{ /*
     OsiSolverInterface *solver = model->solver();
 
     printf("called code %d\n", whereFrom); fflush(stdout);
@@ -24,104 +24,96 @@ static int callBack(CbcModel * model, int whereFrom)
     fflush( stdout );
     }*/
 
-    return 0;
+  return 0;
 }
 
 class SolHandler : public CbcEventHandler {
-    public:
-        virtual CbcAction event(CbcEvent whichEvent);
-        SolHandler();
-        SolHandler(CbcModel * model);
-        virtual ~SolHandler();
-        SolHandler(const SolHandler & rhs);
-        SolHandler& operator=(const SolHandler& rhs);
-        virtual CbcEventHandler *clone() const ;
+public:
+  virtual CbcAction event(CbcEvent whichEvent);
+  SolHandler();
+  SolHandler(CbcModel *model);
+  virtual ~SolHandler();
+  SolHandler(const SolHandler &rhs);
+  SolHandler &operator=(const SolHandler &rhs);
+  virtual CbcEventHandler *clone() const;
 };
 
-SolHandler::SolHandler () 
-    : CbcEventHandler()
+SolHandler::SolHandler()
+  : CbcEventHandler()
 {
 }
 
-SolHandler::SolHandler (const SolHandler & rhs) 
-    : CbcEventHandler(rhs)
-{  
-}
-
-SolHandler::SolHandler(CbcModel * model)
-    : CbcEventHandler(model)
+SolHandler::SolHandler(const SolHandler &rhs)
+  : CbcEventHandler(rhs)
 {
 }
 
-SolHandler::~SolHandler ()
+SolHandler::SolHandler(CbcModel *model)
+  : CbcEventHandler(model)
 {
 }
 
-SolHandler &SolHandler::operator=(const SolHandler& rhs)
+SolHandler::~SolHandler()
 {
-    if (this != &rhs) {
-        CbcEventHandler::operator=(rhs);
-    }
-    return *this;
 }
 
-CbcEventHandler * SolHandler::clone() const
+SolHandler &SolHandler::operator=(const SolHandler &rhs)
 {
-    return new SolHandler(*this);
+  if (this != &rhs) {
+    CbcEventHandler::operator=(rhs);
+  }
+  return *this;
+}
+
+CbcEventHandler *SolHandler::clone() const
+{
+  return new SolHandler(*this);
 }
 
 CbcEventHandler::CbcAction SolHandler::event(CbcEvent whichEvent)
 {
-    // If in sub tree carry on
-    if (!model_->parentModel()) 
-    {
-        if (whichEvent==solution||whichEvent==heuristicSolution) 
-        {
-            char str[256] = " NOT preproc";
-            OsiSolverInterface *solver = model_->solver();
-            if (solver->getPreProcessor())
-            {
-                CglPreProcess *cgp = (CglPreProcess *)solver->getPreProcessor();
-                OsiSolverInterface *orig = cgp->originalModel();
-                strcpy( str, " PREPROC ");
-                char strp[256] = "";
-                sprintf(strp, " vars from %d to %d", orig->getNumCols(), solver->getNumCols());
-                strcpy( str, strp );
-            }
+  // If in sub tree carry on
+  if (!model_->parentModel()) {
+    if (whichEvent == solution || whichEvent == heuristicSolution) {
+      char str[256] = " NOT preproc";
+      OsiSolverInterface *solver = model_->solver();
+      if (solver->getPreProcessor()) {
+        CglPreProcess *cgp = (CglPreProcess *)solver->getPreProcessor();
+        OsiSolverInterface *orig = cgp->originalModel();
+        strcpy(str, " PREPROC ");
+        char strp[256] = "";
+        sprintf(strp, " vars from %d to %d", orig->getNumCols(), solver->getNumCols());
+        strcpy(str, strp);
+      }
 
+      printf("EVH : %s\n", str);
+      fflush(stdout);
+    }
+  }
 
-            printf("EVH : %s\n", str); fflush(stdout);
-        }
-   }
-
-    return noAction;
+  return noAction;
 }
 
-
-
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-    if (argc<2)
-    {
-        fprintf( stderr, "enter instance name\n" );
-        exit( EXIT_FAILURE );
-    }
+  if (argc < 2) {
+    fprintf(stderr, "enter instance name\n");
+    exit(EXIT_FAILURE);
+  }
 
-    CbcSolverUsefulData cbcData;
-    cbcData.noPrinting_ = false;
-    OsiClpSolverInterface lp;
+  CbcSolverUsefulData cbcData;
+  cbcData.noPrinting_ = false;
+  OsiClpSolverInterface lp;
 
-    lp.readMps(argv[1]);
+  lp.readMps(argv[1]);
 
-    CbcModel model(lp);
+  CbcModel model(lp);
 
-    SolHandler sh;
-    model.passInEventHandler( &sh );
+  SolHandler sh;
+  model.passInEventHandler(&sh);
 
-    CbcMain0( model, cbcData );
-    CbcMain1(argc-1, (const char **)(argv+1), model, callBack, cbcData );
+  CbcMain0(model, cbcData);
+  CbcMain1(argc - 1, (const char **)(argv + 1), model, callBack, cbcData);
 
-
-
-    return 0;
+  return 0;
 }
